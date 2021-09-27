5e33c61496bf3e800a86111841283d6f5b828f8a
vanyabeat <vanyabeat@yandex.ru>
2021-09-27 21:02:01 +0300

    Issues

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <cmath>
 
+
+/// запрещено использовать пространство имен std:: глобально
 using namespace std;
 
+/// Учите слово длинна
+/// функции в языке называем стилем under_score
 int LenghtString(const char mass[]){
     int i = 0;
     while (mass[i]){
@@ -12,11 +16,16 @@ int LenghtString(const char mass[]){
 }
 
 int main(int argc, char *argv[]) {
+/// Лишгий комментарий
 //    cout << strlen(argv[1]) << endl; // можно и таким образом посчитать длинну строки
     int summa_v_dec = 0;
+    /// плохое название переменной
     int sixt_in_dec[argc - 1];
     int sign = 1;
     for (int i = 1; i < argc; i++){
+        /// выделите функционал в отдельную функцию
+        /// алгоритм не верный
+        /// функционал выделить в отдельную функцию в пространстве имен BMTSU
         int symbol_in_int[LenghtString(argv[i])];
         int c = 0;
         int dec_number = 0;
@@ -25,17 +34,22 @@ int main(int argc, char *argv[]) {
                 sign = -1;
                 c++;
             }
+            /// не пишите коды символов, происходит неявное привидение типов
+            /// -0 wtf
             else if ((48 <= argv[i][c] - 0) && (argv[i][c] - 0 <= 57)){
                 symbol_in_int[n] = argv[i][c] - 48;
+                /// нужно обойтись без возведения в степень
                 dec_number += symbol_in_int[n] * pow(16, n); // pow(что возвести, во что возчести)
                 c++;
             }
+            /// тут почему-то вы используете символы
             else if ((0 <= argv[i][c] - 'A') && (argv[i][c] - 'A' <= 5)){
                 symbol_in_int[n] = argv[i][c] - 'A' + 10;
                 dec_number += symbol_in_int[n] * pow(16, n);
                 c++;
             }
             else{
+                /// по условию задачи числа на вход попадают корректные
                 cout << "This number isn't in 16 system" << endl;
             }
         }
@@ -43,13 +57,16 @@ int main(int argc, char *argv[]) {
         sixt_in_dec[i] = dec_number * sign;
         summa_v_dec += dec_number;
     }
+    /// обявите ваш номер варианта в глобальную константную переменную
     for (int i = 1; i < argc; ++i){
         cout << sixt_in_dec[i] + 111 << endl;
     }
+    /// ненужный комментарий
     //cout << sixt_in_dec[argc - 1] << "=" << summa_v_dec << endl; // можно вывести сумму вводимых чисел
     //cout << (sizeof (sic_in_dec))/sizeof(sic_in_dec[0]) << endl; // вычисление длинны маассива
     return 0;
 }
+/// Глупые и не нужные комментарии, в следущий раз если такое будет присутствовать в коде, то остальное проверять не буду
 //В первый день зимы
 //На краешке Земли
 //Нечаянно расстались мы с тобой
